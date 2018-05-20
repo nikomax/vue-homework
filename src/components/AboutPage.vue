77b8121504fb908b1376c95396e4ba6ca0933350
nikomax <maks.chuprina@gmail.com>
2018-05-20 15:40:56 +0300

    upd

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,34 +1,65 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="main">
+    <div class="container">
+      <h2>What We Are All About</h2>
+      <div class="text">
+        <p>
+          It’s not always easy to filter out noise and purely focus on the content. Too many blogs and news sites are
+          filled with colourful and distracting
+          advertisement left right and centre. This is where we do things differently! Rather than just focusing purely
+          and generating revenue, we focus
+          on great user experience.
+        </p>
+        <p>
+          You will notice that our news articles are content focused and free of any distractions. On top of that, we
+          deliver content revolving design on a
+          daily basis. Offering you insightful tutorials, techniques, design inspirations, mobile app development,
+          coding, unique advertisements, and
+          freebies.
+        </p>
+        <p>
+          Our focus is and forever will be to bring high quality resources to lean from and to be inspired. Visit us
+          daily for new design related articles!
+        </p>
+      </div>
+      <hr>
+      <h2>Meet Our Team!</h2>
+      <div class="team">
+        <div class="team__item">
+          <team-item/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'AboutPage',
-  data () {
-    return {
-      msg: 'Hello from About'
+  import TeamItem from '@/components/TeamItem'
+  export default {
+    name: 'AboutPage',
+    components: {
+      TeamItem
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
---
+++ src/components/TeamItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="team-item">
+    <div class="team-item__avatar">
+      <img src="" alt="">
+    </div>
+    <div class="team-item__name">
+
+    </div>
+    <div class="team-item__position">
+
+    </div>
+    <div class="team-item__desc">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    props: ['teamAvatar', 'teamName', 'teamPosition', 'teamDesc'],
+  };
+</script>
+
+<style lang="sass">
+
+</style>
